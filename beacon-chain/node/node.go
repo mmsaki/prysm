4566a6321f7556f68876001deec1fd3845fd8b7c
Nishant Das <nishdas93@gmail.com>
2024-05-13 16:08:39 +0800

    Enable E2E For PeerDAS (#13945)
    * Enable E2E And Add Fixes
    
    * Register Same Topic For Data Columns
    
    * Initialize Capacity Of Slice
    
    * Fix Initialization of Data Column Receiver
    
    * Remove Mix In From Merkle Proof
    
    * E2E: Subscribe to all subnets.
    
    * Remove Index Check
    
    * Remaining Bug Fixes to Get It Working
    
    * Change Evaluator to Allow Test to Finish
    
    * Fix Build
    
    * Add Data Column Verification
    
    * Fix LoopVar Bug
    
    * Do Not Allocate Memory
    
    * Update beacon-chain/blockchain/process_block.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Update beacon-chain/core/peerdas/helpers.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Update beacon-chain/core/peerdas/helpers.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Gofmt
    
    * Fix It Again
    
    * Fix Test Setup
    
    * Fix Build
    
    * Fix Trusted Setup panic
    
    * Fix Trusted Setup panic
    
    * Use New Test
    
    ---------
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>

--- beacon-chain/node/node.go
+++ beacon-chain/node/node.go
@@ -969,6 +969,7 @@ func (b *BeaconNode) registerRPCService(router *http.ServeMux) error {
 		FinalizationFetcher:       chainService,
 		BlockReceiver:             chainService,
 		BlobReceiver:              chainService,
+		DataColumnReceiver:        chainService,
 		AttestationReceiver:       chainService,
 		GenesisTimeFetcher:        chainService,
 		GenesisFetcher:            chainService,
