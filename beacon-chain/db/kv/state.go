2f3cf1e3f7369a66c4fe5a0831bae3d9744eee69
Potuz <potuz@prysmaticlabs.com>
2024-11-06 07:21:15 -0300

    fix pruning

--- beacon-chain/db/kv/state.go
+++ beacon-chain/db/kv/state.go
@@ -711,7 +711,7 @@ func marshalState(ctx context.Context, st state.ReadOnlyBeaconState) ([]byte, er
 			return nil, err
 		}
 		return snappy.Encode(nil, append(electraKey, rawObj...)), nil
-	case *ethpb.BeaconStateEPBS:
+	case version.EPBS:
 		rState, ok := st.ToProtoUnsafe().(*ethpb.BeaconStateEPBS)
 		if !ok {
 			return nil, errors.New("non valid inner state")
